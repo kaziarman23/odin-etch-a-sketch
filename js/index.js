2d19c71358b6f12ddad5d0fa465889e8b696c4e1
kaziarman23 <kaziarman@proton.me>
2024-01-31 10:06:14 +0600

    project-done

---
+++ js/index.js
@@ -0,0 +1,64 @@
+let color ="black"
+
+document.addEventListener("DOMContentLoaded",function (){
+    createBoard(16);
+    
+    let popup_btn =document.querySelector("#popup")
+    popup_btn.addEventListener("click",function (){
+        let size = getSize()
+        createBoard(size);
+    })
+})
+
+
+function createBoard(size){
+    let board = document.querySelector(".contener");
+
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+
+    let numDiv = size * size;
+
+    for(let i = 0; i < numDiv; i++){
+    let div =document.createElement("div");
+    board.insertAdjacentElement("beforeend", div)
+    div.addEventListener("mouseover", colorDiv)
+    }
+}
+
+
+
+function getSize(){
+    let input =prompt("what will be the size of the board")
+    let message =document.querySelector("#message")
+    if (input === ""){
+        message.innerText ="please provide a number";
+    }
+    else if(input < 0 || input > 100){
+        message.innerText ="please provide a number between 1 and 100";
+    }
+    else{
+        message.innerText ="Now you can play";
+        return input
+    }
+}
+
+
+function setColor(colorChoice){
+     color = colorChoice
+}
+
+function colorDiv(){
+    if(color === "Random"){
+        this.style.backgroundColor = `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`
+    }
+    else{
+        this.style.backgroundColor ="black"
+    }
+}
+
+function resetBoard(){
+    let divs =document.querySelectorAll("div")
+    divs.forEach( (div) =>div.style.backgroundColor ="white");
+}
